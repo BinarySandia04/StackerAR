9995ddf56c6b94812b01c0333a27a7800c839417
BinarySandia04 <aranseraroig@gmail.com>
2021-04-29 19:55:32 +0200

    First commit B-)

---
+++ Assets/Arte/Scripts/CoreGame.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public float diff = 3f;
+    public float hueInc = 0.05f;
+    public float upModifier = 0.05f;
+    int height = 0;
+
+    public int tileNumber = 0;
+
+    public Transform root;
+    public GameObject droppingTilePrefab;
+
+    public Transform spawnerX, spawnerZ, limitX, limitZ;
+
+    private float currentHue = 0;
+
+    private float startDif;
+
+    StackerCube currentTile;
+
+    Vector3 startPos;
+
+    void Start()
+    {
+        startDif = diff;
+        startPos = transform.position;
+        currentTile = CreateTile((int)Random.Range(0, 2));
+    }
+
+    public void Reset(){
+        foreach(Transform t in root){
+            Destroy(t.gameObject);
+        }
+
+        tileNumber = 0;
+        currentTile = CreateTile((int)Random.Range(0, 2));
+        currentHue = 0;
+        diff = startDif;
+
+        transform.position = startPos;
+        
+        height = 0;
+    }
+
+    StackerCube CreateTile(int dir){
+        GameObject inst = Instantiate(droppingTilePrefab, new Vector3(1000,1000,1000), Quaternion.identity) as GameObject;
+        inst.transform.parent = root;
+        StackerCube cube = inst.GetComponent<StackerCube>();
+
+        Vector3 from, to;
+
+        if(dir == 0){
+            // Use X
+            from = spawnerX.position;
+            to = limitX.position;
+        } else {
+            // Use Z
+            from = spawnerZ.position;
+            to = limitZ.position;
+        }
+
+        Debug.Log(cube);
+        cube.StartMoving(from, to, diff, Color.HSVToRGB(currentHue % 1, 1, 1));
+        Debug.Log("What");
+
+        currentHue += hueInc;
+        diff -= 0.025f;
+
+        return cube;
+    }
+
+    void TryDropTile() {
+        tileNumber++;
+        currentTile.Drop();
+        currentTile = null;
+
+        transform.position += Vector3.up * upModifier;
+        currentTile = CreateTile(tileNumber % 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touches.Length > 0){
+            Touch t = Input.touches[0];
+            if(t.phase == TouchPhase.Ended){
+                TryDropTile();
+            }
+        }
+    }
+}
---
+++ Assets/Arte/Scripts/StackerCube.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackerCube : MonoBehaviour
+{
+    public Vector3 from, to;
+    float time = 5;
+
+    public Shader sha;
+
+    bool moving = false;
+
+    bool going = false;
+
+    public bool dropping = false;
+
+    Rigidbody rb;
+
+    MeshRenderer rend;
+    public void StartMoving(Vector3 from, Vector3 to, float time, Color col){
+        rend = GetComponent<MeshRenderer>();
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+
+        this.from = from;
+        this.to = to;
+        this.time = time;
+
+        Debug.Log("ASSIGNED!!!");
+
+        rend.sharedMaterial.color = col;
+
+        rend.enabled = true;
+        moving = true;
+
+        Debug.Log("AAA");
+    }
+
+    public void Drop(){
+        Debug.Log("DROPPIN!");
+
+        dropping = true;
+
+        Debug.Log(dropping);
+
+        rb.useGravity = true;
+    }
+
+    // Update is called once per frame
+    float f = 0;
+
+    void Update()
+    {
+        if(!dropping){
+            if(!going){
+                if(f < 1){
+                    f += Time.deltaTime / time;
+                } else {
+                    going = !going;
+                }
+            } else {
+                if(f > 0){
+                    f -= Time.deltaTime / time;
+                } else {
+                    going = !going;
+                }
+            }
+
+            Debug.Log(from + " " + to);
+            Debug.Log(f);
+
+            Vector3 desiredPos = Vector3.Lerp(from, to, f);
+            Debug.Log(desiredPos);
+            transform.position = desiredPos;
+        }
+    }
+}
